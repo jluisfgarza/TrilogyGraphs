971033bded06239f9974062ada08eb29e9434847
Elí Emmanuel Linares Romero <eli.emmanuel01@gmail.com>
2017-11-29 06:56:09 -0600

    Data was refined for the application purposes. It now shows percentages about its context per state in the right order of presentation. Some Scaling controls were redefined. 1,2,3 changes the context being analyzed, and 4 to 9 does all the scaling

--- Project 3 OpenGL Project/main.cpp
+++ Project 3 OpenGL Project/main.cpp
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <vector>
 #include <math.h>
+#include <fstream>
 
 #ifdef __APPLE__
 #include <GLUT/glut.h>
@@ -23,7 +24,7 @@
 
 #include <stdlib.h>
 #include <math.h>
-#include <stdio.h>
+
 #include "SOIL.h"
 
 using namespace std;
@@ -36,37 +37,80 @@ float rotateX = 0.0f, rotateY = 0.0f, rotateZ = 0.0f;
 float translateX = 0.0f, translateY = 0.0f, translateZ = 0.0f;
 float scaleX = 1.0f, scaleY = 1.0f, scaleZ = 1.0f;
 float scalingFactor = 1.0f;
+
 GLuint tex_2d;
 
+// We are showing 3 context variables for the 32 states
+int intContext = 0; // Only values 0, 1, or 2
+float STATES[3][32];
 
 void init(void)
 {
     glClearColor(0,0,0,0);
     //glClearColor(1, 1, 1, 1);
-
+    
     // load an image file directly as a new OpenGL texture
     tex_2d = SOIL_load_OGL_texture
     (
-        //"D:/Desktop/Github/TrilogyGraphs/Project 3 OpenGL Project/mexico.png",
-        "/Users/elielr01/Documents/9no Semestre ITC/Gráficas Computacionales/Prueba/Prueba/mexico.png",
-        SOIL_LOAD_AUTO,
-        SOIL_CREATE_NEW_ID,
-        SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
-    );
-
+     "/Users/elielr01/Documents/9no Semestre ITC/Gráficas Computacionales/Prueba/Prueba/mexico.png",
+     SOIL_LOAD_AUTO,
+     SOIL_CREATE_NEW_ID,
+     SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+     );
+    
     //check for an error during the load process
     if( 0 == tex_2d )
     {
-    printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
+        printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
     }
-
+    
+    // We load population data
+    ifstream popFile;
+    popFile.open("/Users/elielr01/Documents/9no Semestre ITC/Gráficas Computacionales/Prueba/Prueba/population.txt");
+    int index = 0;
+    if (popFile.is_open()) {
+        while (!popFile.eof()) {
+            popFile >> STATES[0][index];
+            cout << STATES[0][index] << "\n";
+            index++;
+        }
+    }
+    popFile.close();
+    
+    // We load unemployment data
+    ifstream unemployFile;
+    unemployFile.open("/Users/elielr01/Documents/9no Semestre ITC/Gráficas Computacionales/Prueba/Prueba/unemployment.txt");
+    index = 0;
+    if (unemployFile.is_open()) {
+        while (!unemployFile.eof()) {
+            unemployFile >> STATES[1][index];
+            //cout << STATES[1][index] << "\n";
+            index++;
+        }
+    }
+    unemployFile.close();
+    
+    // We load the poverty data
+    ifstream povertyFile;
+    povertyFile.open("/Users/elielr01/Documents/9no Semestre ITC/Gráficas Computacionales/Prueba/Prueba/poverty.txt");
+    index = 0;
+    if (povertyFile.is_open()) {
+        while (!povertyFile.eof()) {
+            povertyFile >> STATES[2][index];
+            //cout << STATES[2][index] << "\n";
+            index++;
+        }
+    }
+    povertyFile.close();
+    
+    
 }
 
 /*
  Function that draws a bar within the cube.
-
+ 
  Let's keep it to a grid of 3x3 where we manage coordinates (x,z) as:
-
+ 
  ----------------------------------------
  |            |            |              |
  | (-0.6,-0.6)| (0.1,-0.6) |  (0.8,-0.6)  |
@@ -80,70 +124,72 @@ void init(void)
  | (-0.6,0.8) | (0.1,0.8)  |  (0.8,0.8)   |
  |            |            |              |
  ----------------------------------------
-
+ 
  The height should be in a range within 0 and 1.
-
+ 
  The red, green and blue parameters should be also within 0 and 1 and represent RGB
  */
-void drawBar( float x_coordinate, float z_coordinate,float height, float red, float green, float blue){
-
+void drawBar( float x_coordinate, float z_coordinate,float height, float red, float green, float blue, float square_base_size){
+    
     // This is a constant square size defined for our bars
-    float square_base_size = 0.25;
-
+    //float square_base_size = 0.25;
+    
     // We recalculate the height because y axis goes from -1 to 1
-    height = (height * 2.0) - 1.0;
-
+    height = (height * 6.0) - 1.0;
+    
     glBegin(GL_QUADS);        // Draw the bar using QUADS
     glColor3f(red,green,blue);    // Color Blue
-
+    
     glVertex3f( x_coordinate, 1.0f * height, z_coordinate);    // Top Right Of The Quad (Top)
     glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate);    // Top Left Of The Quad (Top)
     glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate - square_base_size);    // Bottom Left Of The Quad (Top)
     glVertex3f( x_coordinate, 1.0f * height, z_coordinate - square_base_size);    // Bottom Right Of The Quad (Top)
-
-
+    
+    
     glVertex3f( x_coordinate,-1.0f, z_coordinate);    // Top Right Of The Quad (Bottom)
     glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate);    // Top Left Of The Quad (Bottom)
     glVertex3f(x_coordinate - square_base_size,-1.0f,z_coordinate - square_base_size);    // Bottom Left Of The Quad (Bottom)
     glVertex3f( x_coordinate,-1.0f,z_coordinate - square_base_size);    // Bottom Right Of The Quad (Bottom)
-
-
+    
+    
     glVertex3f( x_coordinate, 1.0f * height, z_coordinate);    // Top Right Of The Quad (Front)
     glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate);    // Top Left Of The Quad (Front)
     glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate);    // Bottom Left Of The Quad (Front)
     glVertex3f( x_coordinate,-1.0f, z_coordinate);    // Bottom Right Of The Quad (Front)
-
-
+    
+    
     glVertex3f( x_coordinate,-1.0f,z_coordinate - square_base_size);    // Top Right Of The Quad (Back)
     glVertex3f(x_coordinate - square_base_size,-1.0f,z_coordinate - square_base_size);    // Top Left Of The Quad (Back)
     glVertex3f(x_coordinate - square_base_size, 1.0f * height,z_coordinate - square_base_size);    // Bottom Left Of The Quad (Back)
     glVertex3f( x_coordinate, 1.0f * height,z_coordinate - square_base_size);    // Bottom Right Of The Quad (Back)
-
-
+    
+    
     glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate);    // Top Right Of The Quad (Left)
     glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate - square_base_size);    // Top Left Of The Quad (Left)
     glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate - square_base_size);    // Bottom Left Of The Quad (Left)
     glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate);    // Bottom Right Of The Quad (Left)
-
-
+    
+    
     glVertex3f( x_coordinate, 1.0f * height, z_coordinate - square_base_size);    // Top Right Of The Quad (Right)
     glVertex3f( x_coordinate, 1.0f * height, z_coordinate);    // Top Left Of The Quad (Right)
     glVertex3f( x_coordinate,-1.0f, z_coordinate);    // Bottom Left Of The Quad (Right)
     glVertex3f( x_coordinate,-1.0f, z_coordinate - square_base_size);    // Bottom Right Of The Quad (Right)
-
+    
     glEnd();            // End drawing bar
-
+    
 }
 
 void drawStates() {
     
-    float height = 1;
+    //float height = 1;
     float red = 1;
     float green = 0;
     float blue = 0;
     
+    int intStateIndex = 0;
+    
     //Baja California Norte
-    drawBar(-12, -6, height, red, green, blue, 0.4);
+    drawBar(-12, -6, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -155,7 +201,7 @@ void drawStates() {
     }
     
     //Baja California Sur
-    drawBar(-9, -2, height, red, green, blue, 0.4);
+    drawBar(-9, -2, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -167,7 +213,7 @@ void drawStates() {
     }
     
     //Sonora
-    drawBar(-7.7, -5.1, height, red, green, blue, 0.5);
+    drawBar(-7.7, -5.1, STATES[intContext][intStateIndex++], red, green, blue, 0.5);
     
     if (red == 0.0) {
         red = 1.0;
@@ -179,7 +225,7 @@ void drawStates() {
     }
     
     //Chihuahua
-    drawBar(-3.7, -4.2, height, red, green, blue, 0.5);
+    drawBar(-3.7, -4.2, STATES[intContext][intStateIndex++], red, green, blue, 0.5);
     
     if (red == 0.0) {
         red = 1.0;
@@ -191,7 +237,7 @@ void drawStates() {
     }
     
     //Coahuila
-    drawBar(0.5, -2.2, height, red, green, blue, 0.5);
+    drawBar(0.5, -2.2, STATES[intContext][intStateIndex++], red, green, blue, 0.5);
     
     if (red == 0.0) {
         red = 1.0;
@@ -203,7 +249,7 @@ void drawStates() {
     }
     
     //Nuevo Leon
-    drawBar(2.8, -1.3, height, red, green, blue, 0.3);
+    drawBar(2.8, -1.3, STATES[intContext][intStateIndex++], red, green, blue, 0.3);
     
     if (red == 0.0) {
         red = 1.0;
@@ -215,7 +261,7 @@ void drawStates() {
     }
     
     //Tamaulipas
-    drawBar(4, 0.5, height, red, green, blue, 0.4);
+    drawBar(4, 0.5, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -227,7 +273,7 @@ void drawStates() {
     }
     
     // Sinaloa
-    drawBar(-4.3, -0.3, height, red, green, blue, 0.3);
+    drawBar(-4.3, -0.3, STATES[intContext][intStateIndex++], red, green, blue, 0.3);
     
     if (red == 0.0) {
         red = 1.0;
@@ -239,7 +285,7 @@ void drawStates() {
     }
     
     //Durango
-    drawBar(-2.1, -0.3, height, red, green, blue, 0.4);
+    drawBar(-2.1, -0.3, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -251,7 +297,7 @@ void drawStates() {
     }
     
     //Zacatecas
-    drawBar(-0.4, 0.7, height, red, green, blue, 0.4);
+    drawBar(-0.4, 0.7, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -263,7 +309,7 @@ void drawStates() {
     }
     
     //San Luis Potosi
-    drawBar(1.8, 0.9, height, red, green, blue, 0.4);
+    drawBar(1.8, 0.9, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -275,7 +321,7 @@ void drawStates() {
     }
     
     //Nayarit
-    drawBar(-2.4, 2, height, red, green, blue, 0.3);
+    drawBar(-2.4, 2, STATES[intContext][intStateIndex++], red, green, blue, 0.3);
     
     if (red == 0.0) {
         red = 1.0;
@@ -287,7 +333,7 @@ void drawStates() {
     }
     
     //Aguascalientes
-    drawBar(0.2, 1.9, height, red, green, blue, 0.2);
+    drawBar(0.2, 1.9, STATES[intContext][intStateIndex++], red, green, blue, 0.2);
     
     if (red == 0.0) {
         red = 1.0;
@@ -299,7 +345,7 @@ void drawStates() {
     }
     
     //Jalisco
-    drawBar(-1.8, 4, height, red, green, blue, 0.4);
+    drawBar(-1.8, 4, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -311,7 +357,7 @@ void drawStates() {
     }
     
     //Guanajuato
-    drawBar(1.4, 3.1, height, red, green, blue, 0.3);
+    drawBar(1.4, 3.1, STATES[intContext][intStateIndex++], red, green, blue, 0.3);
     
     if (red == 0.0) {
         red = 1.0;
@@ -323,7 +369,7 @@ void drawStates() {
     }
     
     //Querétaro
-    drawBar(2.6, 3.1, height, red, green, blue, 0.2);
+    drawBar(2.6, 3.1, STATES[intContext][intStateIndex++], red, green, blue, 0.2);
     
     if (red == 0.0) {
         red = 1.0;
@@ -335,7 +381,7 @@ void drawStates() {
     }
     
     //Hidalgo
-    drawBar(3.6, 3.2, height, red, green, blue, 0.25);
+    drawBar(3.6, 3.2, STATES[intContext][intStateIndex++], red, green, blue, 0.25);
     
     if (red == 0.0) {
         red = 1.0;
@@ -347,7 +393,7 @@ void drawStates() {
     }
     
     //Veracruz
-    drawBar(6, 5.1, height, red, green, blue, 0.3);
+    drawBar(6, 5.1, STATES[intContext][intStateIndex++], red, green, blue, 0.3);
     
     if (red == 0.0) {
         red = 1.0;
@@ -359,7 +405,7 @@ void drawStates() {
     }
     
     //Michoacán
-    drawBar(1, 4.7, height, red, green, blue, 0.3);
+    drawBar(1, 4.7, STATES[intContext][intStateIndex++], red, green, blue, 0.3);
     
     if (red == 0.0) {
         red = 1.0;
@@ -371,7 +417,7 @@ void drawStates() {
     }
     
     //Colima
-    drawBar(-1.5, 4.9, height, red, green, blue, 0.2);
+    drawBar(-1.5, 4.9, STATES[intContext][intStateIndex++], red, green, blue, 0.2);
     
     if (red == 0.0) {
         red = 1.0;
@@ -383,7 +429,7 @@ void drawStates() {
     }
     
     //Edo. México
-    drawBar(2.8, 4.6, height, red, green, blue, 0.2);
+    drawBar(2.8, 4.6, STATES[intContext][intStateIndex++], red, green, blue, 0.2);
     
     if (red == 0.0) {
         red = 1.0;
@@ -395,7 +441,7 @@ void drawStates() {
     }
     
     // DF
-    drawBar(3.3, 4.8, height, red, green, blue, 0.1);
+    drawBar(3.3, 4.8, STATES[intContext][intStateIndex++], red, green, blue, 0.2);
     
     if (red == 0.0) {
         red = 1.0;
@@ -407,7 +453,7 @@ void drawStates() {
     }
     
     // Morelos
-    drawBar(3.3, 5.3, height, red, green, blue, 0.15);
+    drawBar(3.3, 5.3, STATES[intContext][intStateIndex++], red, green, blue, 0.15);
     
     if (red == 0.0) {
         red = 1.0;
@@ -419,7 +465,7 @@ void drawStates() {
     }
     
     // Tlaxcala
-    drawBar(4.4, 4.5, height, red, green, blue, 0.15);
+    drawBar(4.4, 4.5, STATES[intContext][intStateIndex++], red, green, blue, 0.15);
     
     if (red == 0.0) {
         red = 1.0;
@@ -431,7 +477,7 @@ void drawStates() {
     }
     
     // Puebla
-    drawBar(4.4, 5.5, height, red, green, blue, 0.25);
+    drawBar(4.4, 5.5, STATES[intContext][intStateIndex++], red, green, blue, 0.25);
     
     if (red == 0.0) {
         red = 1.0;
@@ -443,7 +489,7 @@ void drawStates() {
     }
     
     // Guerrero
-    drawBar(2.5, 6.3, height, red, green, blue, 0.4);
+    drawBar(2.5, 6.3, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -455,7 +501,7 @@ void drawStates() {
     }
     
     // Oaxaca
-    drawBar(6.2, 6.9, height, red, green, blue, 0.4);
+    drawBar(6.2, 6.9, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -467,7 +513,7 @@ void drawStates() {
     }
     
     // Yucatan
-    drawBar(13.8, 3.2, height, red, green, blue, 0.4);
+    drawBar(13.8, 3.2, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -479,7 +525,7 @@ void drawStates() {
     }
     
     // Quintana Roo
-    drawBar(14.2, 4.8, height, red, green, blue, 0.4);
+    drawBar(14.2, 4.8, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -491,7 +537,7 @@ void drawStates() {
     }
     
     // Campeche
-    drawBar(12.3, 5.2, height, red, green, blue, 0.4);
+    drawBar(12.3, 5.2, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -503,7 +549,7 @@ void drawStates() {
     }
     
     // Tabasco
-    drawBar(9.5, 6, height, red, green, blue, 0.2);
+    drawBar(9.5, 6, STATES[intContext][intStateIndex++], red, green, blue, 0.2);
     
     if (red == 0.0) {
         red = 1.0;
@@ -515,7 +561,7 @@ void drawStates() {
     }
     
     // Chiapas
-    drawBar(10.5, 7.5, height, red, green, blue, 0.4);
+    drawBar(10.5, 7.5, STATES[intContext][intStateIndex++], red, green, blue, 0.4);
     
     if (red == 0.0) {
         red = 1.0;
@@ -528,19 +574,19 @@ void drawStates() {
 }
 
 void sceneRenderer(void) {
-
+    
     glMatrixMode(GL_MODELVIEW);
     // Clear color and depth buffers.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // Reset transformation matrix.
     glLoadIdentity();
-
+    
     // Set up the camera.
     gluLookAt(truckX, truckY, truckZ, dollyX, dollyY, dollyZ, boomX, boomY, boomZ);
     glTranslatef(translateX, translateY, translateZ);
     //glTranslatef(0.0,-1,-5);
     glTranslatef(0.0,-1,5);
-
+    
     //glPushMatrix();
     glRotatef(rotateX, 1.0f, 0.0f, 0.0f);
     glRotatef(rotateY, 0.0f, 1.0f, 0.0f);
@@ -548,30 +594,31 @@ void sceneRenderer(void) {
     glScalef(scaleX, scaleY, scaleZ);
     
     //glRotatef(20, 1, 0, 0);
-
+    
+    
     glPushMatrix();
-
+    
     /* Draw Mexico Map */
     glEnable(GL_TEXTURE_2D);
     glBindTexture (GL_TEXTURE_2D, tex_2d);
-
+    
     glBegin(GL_QUADS);
     glColor4f(1,1,1,1);
-    glVertex3f(10,-1.0f, 10);    // Front Right Quad
+    glVertex3f(16,-1.0f, 10);    // Front Right Quad
     glTexCoord2f(1,0 );
-    glVertex3f(-10,-1.0f, 10);    // Front Left Quad
+    glVertex3f(-16,-1.0f, 10);    // Front Left Quad
     glTexCoord2f(0, 0);
-    glVertex3f(-10,-1.0f,-10);    // Back Right Quad
+    glVertex3f(-16,-1.0f,-10);    // Back Right Quad
     glTexCoord2f(0, 1);
-    glVertex3f(10,-1.0f,-10);    // Back Left Quad
+    glVertex3f(16,-1.0f,-10);    // Back Left Quad
     glTexCoord2f(1, 1);
     glEnd();
-
+    
     glDisable(GL_TEXTURE_2D);
     
     //Draw the bars per states
     drawStates();
-
+    
     glFlush();
     glutSwapBuffers();
 }
@@ -581,19 +628,19 @@ void changeWindowSize(int width, int height) {
     if (height == 0) {
         height = 1;
     }
-
+    
     float ratio = width * (1.0 / height);
-
+    
     // Use and reset the projection matrix.
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-
+    
     // Set the viewport to the entire window.
     glViewport(0, 0, width, height);
-
+    
     // Set the right perspective.
     gluPerspective(45.0f, ratio, 0.1f, 100.0f);
-
+    
     // Bring back the model view.
     glMatrixMode(GL_MODELVIEW);
 }
@@ -623,24 +670,37 @@ void processKeyInput(unsigned char key, int x, int y) {
         case 'd':
             translateX -= 0.9f;
             break;
-            // Scale. DONE
-        case '6':
+            // Scalations. DONE
+            // Scale in X
+        case '5':
             scaleX += 0.1;
             break;
         case '4':
             scaleX -= 0.1;
             break;
-        case '8':
+            // Scale in Y
+        case '7':
             scaleY += 0.1;
             break;
-        case '2':
+        case '6':
             scaleY -= 0.1;
             break;
-        case '7':
+            // Scale in Z
+        case '9':
             scaleZ += 0.1;
             break;
-        case '9':
+        case '8':
             scaleZ -= 0.1;
+            break;
+            // Different contexts. DONE
+        case '1':
+            intContext = 0;
+            break;
+        case '2':
+            intContext = 1;
+            break;
+        case '3':
+            intContext = 2;
             break;
     }
     glutPostRedisplay();
@@ -666,15 +726,15 @@ void ArrowClick(int key, int x, int y) {
 }
 
 int main(int argc, char** argv){
-
+    
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(900, 600);
     glutCreateWindow("TrilogyGraphs");
     init();
-
-
+    
+    
     // Register GLUT callback functions.
     glutReshapeFunc(changeWindowSize);
     glutKeyboardFunc(processKeyInput);
@@ -682,8 +742,8 @@ int main(int argc, char** argv){
     glutDisplayFunc(sceneRenderer);
     glutIdleFunc(sceneRenderer);
     //glutMouseFunc(processMouseInput);
-
-
+    
+    
     // Enter GLUT event processing cycle.
     glutMainLoop();
     return 0;
