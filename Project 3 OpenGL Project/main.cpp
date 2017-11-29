717ea10467c1aeee72092139ae9088bcbf5dfc22
Elí Emmanuel Linares Romero <eli.emmanuel01@gmail.com>
2017-11-29 01:58:01 -0600

    Map added

--- Project 3 OpenGL Project/main.cpp
+++ Project 3 OpenGL Project/main.cpp
@@ -10,6 +10,11 @@
  * is designed to compare geographically distributed scalar data.
  */
 
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <math.h>
+
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -19,78 +24,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*
- Function that draws a bar within the cube.
+#include "SOIL.h"
 
- Let's keep it to a grid of 3x3 where we manage coordinates (x,z) as:
-
- ----------------------------------------
-|            |            |              |
-| (-0.6,-0.6)| (0.1,-0.6) |  (0.8,-0.6)  |
-|            |            |              |
- ----------------------------------------
-|            |            |              |
-| (-0.6,0.1) | (0.1,0.1)  |  (0.8,0.1)   |
-|            |            |              |
- ----------------------------------------
-|            |            |              |
-| (-0.6,0.8) | (0.1,0.8)  |  (0.8,0.8)   |
-|            |            |              |
- ----------------------------------------
-
- The height should be in a range within 0 and 1.
-
- The red, green and blue parameters should be also within 0 and 1 and represent RGB
- */
-void drawBar( float x_coordinate, float z_coordinate,float height, float red, float green, float blue){
-
-    // This is a constant square size defined for our bars
-    float square_base_size = 0.25;
-
-    // We recalculate the height because y axis goes from -1 to 1
-    height = (height * 2.0) - 1.0;
-
-    glBegin(GL_QUADS);        // Draw the bar using QUADS
-    glColor3f(red,green,blue);    // Color Blue
-
-    glVertex3f( x_coordinate, 1.0f * height, z_coordinate);    // Top Right Of The Quad (Top)
-    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate);    // Top Left Of The Quad (Top)
-    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate - square_base_size);    // Bottom Left Of The Quad (Top)
-    glVertex3f( x_coordinate, 1.0f * height, z_coordinate - square_base_size);    // Bottom Right Of The Quad (Top)
-
-
-    glVertex3f( x_coordinate,-1.0f, z_coordinate);    // Top Right Of The Quad (Bottom)
-    glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate);    // Top Left Of The Quad (Bottom)
-    glVertex3f(x_coordinate - square_base_size,-1.0f,z_coordinate - square_base_size);    // Bottom Left Of The Quad (Bottom)
-    glVertex3f( x_coordinate,-1.0f,z_coordinate - square_base_size);    // Bottom Right Of The Quad (Bottom)
-
-
-    glVertex3f( x_coordinate, 1.0f * height, z_coordinate);    // Top Right Of The Quad (Front)
-    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate);    // Top Left Of The Quad (Front)
-    glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate);    // Bottom Left Of The Quad (Front)
-    glVertex3f( x_coordinate,-1.0f, z_coordinate);    // Bottom Right Of The Quad (Front)
-
-
-    glVertex3f( x_coordinate,-1.0f,z_coordinate - square_base_size);    // Top Right Of The Quad (Back)
-    glVertex3f(x_coordinate - square_base_size,-1.0f,z_coordinate - square_base_size);    // Top Left Of The Quad (Back)
-    glVertex3f(x_coordinate - square_base_size, 1.0f * height,z_coordinate - square_base_size);    // Bottom Left Of The Quad (Back)
-    glVertex3f( x_coordinate, 1.0f * height,z_coordinate - square_base_size);    // Bottom Right Of The Quad (Back)
-
-
-    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate);    // Top Right Of The Quad (Left)
-    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate - square_base_size);    // Top Left Of The Quad (Left)
-    glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate - square_base_size);    // Bottom Left Of The Quad (Left)
-    glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate);    // Bottom Right Of The Quad (Left)
-
-
-    glVertex3f( x_coordinate, 1.0f * height, z_coordinate - square_base_size);    // Top Right Of The Quad (Right)
-    glVertex3f( x_coordinate, 1.0f * height, z_coordinate);    // Top Left Of The Quad (Right)
-    glVertex3f( x_coordinate,-1.0f, z_coordinate);    // Bottom Left Of The Quad (Right)
-    glVertex3f( x_coordinate,-1.0f, z_coordinate - square_base_size);    // Bottom Right Of The Quad (Right)
-
-    glEnd();            // End drawing bar
-
-}
+using namespace std;
 
 // Global variables.
 float truckX = 0.0f, truckY = 0.0f, truckZ = 10.0f;
@@ -100,28 +36,146 @@ float rotateX = 0.0f, rotateY = 0.0f, rotateZ = 0.0f;
 float translateX = 0.0f, translateY = 0.0f, translateZ = 0.0f;
 float scaleX = 1.0f, scaleY = 1.0f, scaleZ = 1.0f;
 float scalingFactor = 1.0f;
+GLuint tex_2d;
+
+
+void init(void)
+{
+    glClearColor(0,0,0,0);
+    //glClearColor(1, 1, 1, 1);
+    
+    // load an image file directly as a new OpenGL texture
+    tex_2d = SOIL_load_OGL_texture
+    (
+    "/Users/elielr01/Documents/9no Semestre ITC/Gráficas Computacionales/Prueba/Prueba/mexico.png",
+    SOIL_LOAD_AUTO,
+    SOIL_CREATE_NEW_ID,
+    SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+    );
+
+    //check for an error during the load process
+    if( 0 == tex_2d )
+    {
+    printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
+    }
+    
+}
+
+/*
+ Function that draws a bar within the cube.
+ 
+ Let's keep it to a grid of 3x3 where we manage coordinates (x,z) as:
+ 
+ ----------------------------------------
+ |            |            |              |
+ | (-0.6,-0.6)| (0.1,-0.6) |  (0.8,-0.6)  |
+ |            |            |              |
+ ----------------------------------------
+ |            |            |              |
+ | (-0.6,0.1) | (0.1,0.1)  |  (0.8,0.1)   |
+ |            |            |              |
+ ----------------------------------------
+ |            |            |              |
+ | (-0.6,0.8) | (0.1,0.8)  |  (0.8,0.8)   |
+ |            |            |              |
+ ----------------------------------------
+ 
+ The height should be in a range within 0 and 1.
+ 
+ The red, green and blue parameters should be also within 0 and 1 and represent RGB
+ */
+void drawBar( float x_coordinate, float z_coordinate,float height, float red, float green, float blue){
+    
+    // This is a constant square size defined for our bars
+    float square_base_size = 0.25;
+    
+    // We recalculate the height because y axis goes from -1 to 1
+    height = (height * 2.0) - 1.0;
+    
+    glBegin(GL_QUADS);        // Draw the bar using QUADS
+    glColor3f(red,green,blue);    // Color Blue
+    
+    glVertex3f( x_coordinate, 1.0f * height, z_coordinate);    // Top Right Of The Quad (Top)
+    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate);    // Top Left Of The Quad (Top)
+    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate - square_base_size);    // Bottom Left Of The Quad (Top)
+    glVertex3f( x_coordinate, 1.0f * height, z_coordinate - square_base_size);    // Bottom Right Of The Quad (Top)
+    
+    
+    glVertex3f( x_coordinate,-1.0f, z_coordinate);    // Top Right Of The Quad (Bottom)
+    glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate);    // Top Left Of The Quad (Bottom)
+    glVertex3f(x_coordinate - square_base_size,-1.0f,z_coordinate - square_base_size);    // Bottom Left Of The Quad (Bottom)
+    glVertex3f( x_coordinate,-1.0f,z_coordinate - square_base_size);    // Bottom Right Of The Quad (Bottom)
+    
+    
+    glVertex3f( x_coordinate, 1.0f * height, z_coordinate);    // Top Right Of The Quad (Front)
+    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate);    // Top Left Of The Quad (Front)
+    glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate);    // Bottom Left Of The Quad (Front)
+    glVertex3f( x_coordinate,-1.0f, z_coordinate);    // Bottom Right Of The Quad (Front)
+    
+    
+    glVertex3f( x_coordinate,-1.0f,z_coordinate - square_base_size);    // Top Right Of The Quad (Back)
+    glVertex3f(x_coordinate - square_base_size,-1.0f,z_coordinate - square_base_size);    // Top Left Of The Quad (Back)
+    glVertex3f(x_coordinate - square_base_size, 1.0f * height,z_coordinate - square_base_size);    // Bottom Left Of The Quad (Back)
+    glVertex3f( x_coordinate, 1.0f * height,z_coordinate - square_base_size);    // Bottom Right Of The Quad (Back)
+    
+    
+    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate);    // Top Right Of The Quad (Left)
+    glVertex3f(x_coordinate - square_base_size, 1.0f * height, z_coordinate - square_base_size);    // Top Left Of The Quad (Left)
+    glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate - square_base_size);    // Bottom Left Of The Quad (Left)
+    glVertex3f(x_coordinate - square_base_size,-1.0f, z_coordinate);    // Bottom Right Of The Quad (Left)
+    
+    
+    glVertex3f( x_coordinate, 1.0f * height, z_coordinate - square_base_size);    // Top Right Of The Quad (Right)
+    glVertex3f( x_coordinate, 1.0f * height, z_coordinate);    // Top Left Of The Quad (Right)
+    glVertex3f( x_coordinate,-1.0f, z_coordinate);    // Bottom Left Of The Quad (Right)
+    glVertex3f( x_coordinate,-1.0f, z_coordinate - square_base_size);    // Bottom Right Of The Quad (Right)
+    
+    glEnd();            // End drawing bar
+    
+}
 
 void sceneRenderer(void) {
-
+    
     glMatrixMode(GL_MODELVIEW);
     // Clear color and depth buffers.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // Reset transformation matrix.
     glLoadIdentity();
-
+    
     // Set up the camera.
     gluLookAt(truckX, truckY, truckZ, dollyX, dollyY, dollyZ, boomX, boomY, boomZ);
     glTranslatef(translateX, translateY, translateZ);
     glTranslatef(0.0,0.0,5.5);
-
+    
     //glPushMatrix();
     glRotatef(rotateX, 1.0f, 0.0f, 0.0f);
     glRotatef(rotateY, 0.0f, 1.0f, 0.0f);
     glRotatef(rotateZ, 0.0f, 0.0f, 1.0f);
     glScalef(scaleX, scaleY, scaleZ);
-
+    
     glPushMatrix();
-    glClearColor(1.0, 1.0, 1.0, 0.0);
+    
+    /* Draw Mexico Map */
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture (GL_TEXTURE_2D, tex_2d);
+
+    //glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    //glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glBegin(GL_QUADS);
+    glColor4f(1,1,1,1);
+    glVertex3f(10,-1.0f, 10);    // Front Right Quad
+    glTexCoord2f(1,0 );
+    glVertex3f(-10,-1.0f, 10);    // Front Left Quad
+    glTexCoord2f(0, 0);
+    glVertex3f(-10,-1.0f,-10);    // Back Right Quad
+    glTexCoord2f(0, 1);
+    glVertex3f(10,-1.0f,-10);    // Back Left Quad
+    glTexCoord2f(1, 1);
+    glEnd();
+
+    glDisable(GL_TEXTURE_2D);
+    
     /* Draw the cube canvas */
     glBegin(GL_QUADS);        // Draw partial cube with QUADS
     glColor4f(0.8f,0.8f,0.8f, 1);    // Color Orange
@@ -140,10 +194,10 @@ void sceneRenderer(void) {
     glVertex3f(-1.0f,-1.0f,-1.0f);    // Bottom Left Of The Quad (Left)
     glVertex3f(-1.0f,-1.0f, 1.0f);    // Bottom Right Of The Quad (Left)
     glEnd();            // End drawing partial cube
-
-
+    
+    
     /* Draw Strips */
-
+    
     /* Back Strips */
     float y_strip = 0.75;
     for (int i = 1 ; i <= 7; i++, y_strip-=0.25) {
@@ -155,7 +209,7 @@ void sceneRenderer(void) {
         glVertex3f( 1.0f, y_strip - 0.02,-1.0f);    // Bottom Right Of The Quad (Back)
         glEnd();
     }
-
+    
     /* Left Strips */
     y_strip = 0.75;
     for (int i = 1 ; i <= 7; i++, y_strip-=0.25){
@@ -167,13 +221,13 @@ void sceneRenderer(void) {
         glVertex3f(-1.0f, y_strip - 0.02, 1.0f);    // Bottom Right Of The Quad (Left)
         glEnd();
     }
-
+    
     /* Draw the bars */
     drawBar(0.8, 0.8, 0.8, 1.0, 0.0, 0.0);
     drawBar(0.8, 0.1, 0.25, 0.0, 1.0, 0.0);
     drawBar(0.1, 0.1, 0.5, 0.0, 0.0, 1.0);
     drawBar(-0.6, -0.6, 0.9, 1.0, 1.0, 0.0);
-
+    
     glFlush();
     glutSwapBuffers();
 }
@@ -183,19 +237,19 @@ void changeWindowSize(int width, int height) {
     if (height == 0) {
         height = 1;
     }
-
+    
     float ratio = width * (1.0 / height);
-
+    
     // Use and reset the projection matrix.
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-
+    
     // Set the viewport to the entire window.
     glViewport(0, 0, width, height);
-
+    
     // Set the right perspective.
     gluPerspective(45.0f, ratio, 0.1f, 100.0f);
-
+    
     // Bring back the model view.
     glMatrixMode(GL_MODELVIEW);
 }
@@ -204,28 +258,28 @@ void processKeyInput(unsigned char key, int x, int y) {
     if (key == 27)  // ESC Exit. DONE
         exit(0);
     switch (key) {
-        // Move the camera up or down. DONE
+            // Move the camera up or down. DONE
         case 'r':
             translateY -= 0.9f;
             break;
         case 'f':
             translateY += 0.9f;
             break;
-        // Move the camera forward or backward. DONE
+            // Move the camera forward or backward. DONE
         case 'w':
             translateZ += 0.9f;
             break;
         case 's':
             translateZ -= 0.9f;
             break;
-        // Move the camera left or right. DONE
+            // Move the camera left or right. DONE
         case 'a':
             translateX += 0.9f;
             break;
         case 'd':
             translateX -= 0.9f;
             break;
-        // Scale. DONE
+            // Scale. DONE
         case '6':
             scaleX += 0.1;
             break;
@@ -250,14 +304,14 @@ void processKeyInput(unsigned char key, int x, int y) {
 
 void ArrowClick(int key, int x, int y) {
     switch (key) {
-        // Look left and right. DONE
+            // Look left and right. DONE
         case GLUT_KEY_LEFT :
             dollyX -= 0.9f;
             break;
         case GLUT_KEY_RIGHT :
             dollyX += 0.9f;
             break;
-        // Look up and down. DONE
+            // Look up and down. DONE
         case GLUT_KEY_UP :
             dollyY += 0.9f;
             break;
@@ -268,14 +322,15 @@ void ArrowClick(int key, int x, int y) {
 }
 
 int main(int argc, char** argv){
-
+    
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowPosition(100, 100);
-    glutInitWindowSize(900, 900);
+    glutInitWindowSize(900, 600);
     glutCreateWindow("TrilogyGraphs");
-
-
+    init();
+    
+    
     // Register GLUT callback functions.
     glutReshapeFunc(changeWindowSize);
     glutKeyboardFunc(processKeyInput);
@@ -283,9 +338,10 @@ int main(int argc, char** argv){
     glutDisplayFunc(sceneRenderer);
     glutIdleFunc(sceneRenderer);
     //glutMouseFunc(processMouseInput);
-
-
+    
+    
     // Enter GLUT event processing cycle.
     glutMainLoop();
     return 0;
 }
+
